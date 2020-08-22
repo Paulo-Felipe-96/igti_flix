e6fc295f0302511ea7cda7fc024b14719c72f742
Paulo Martins <paulofelipe0896@gmail.com>
2020-08-22 18:39:37 -0300

    Segundo commit. Introduzindo modelo de dados e primeira migracao

--- genero/forms.py
+++ genero/forms.py
@@ -1,9 +1,8 @@
 from django import forms
+from genero.models import Genero
 
 
 class GeneroForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(GeneroForm, self).__init__(*args, **kwargs)
-        self.fields['descricao'].error_messages = {'required': 'campo obrigatório'}
-
-    descricao = forms.CharField(label='Gênero', required=True)
+    class Meta:
+        model = Genero
+        fields = '__all__'
